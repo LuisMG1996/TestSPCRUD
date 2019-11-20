865f699e90cb80f919524c4048e7bce961497905
Luis Montes! <luis.montesgz@udlap.mx>
2019-11-20 13:47:22 -0600

    Avance JS create

--- js/crudSP.js
+++ js/crudSP.js
@@ -3,21 +3,31 @@ $(function(){
     $("#btnCreate").click(function(){
         //var userid = _spPageContextInfo.userId;
         //var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/web/getuserbyid(" + userid + ")";
+        
+        var title = document.getElementById('Title').value;
+        var nombre = document.getElementById('nombre').value;
+        var date = document.getElementById('fechaNacimiento').value;
+        var number = document.getElementById('telefono').value;
+
         var requestUri = siteURl + "/_api/lists/getByTitle('" + listTitle + "')";
         var requestHeaders = { 
             "accept": "application/json;odata=verbose",
             '__metadata': {
                 'type': 'SP.Data.listname.ListItem'
             },
-            'Title': 'MyItem'
+            'Title': + title,
+            'Nombre': + nombre,
+            'fechaNacimiento': + fechaNacimiento,
+            'Number': + number 
           };  
-    $.ajax({
+    print(requestHeaders);
+    /*$.ajax({
         url: requestUri,
         contentType: "application/json;odata=verbose",
         headers: requestHeaders,
         success: onSuccess,        
         error: onError    
-    }); 
+    }); */
     function onSuccess(data, request) {        
         var userinfo = data.d;               
         alert ('Display Name: '+ userinfo.Title);  
