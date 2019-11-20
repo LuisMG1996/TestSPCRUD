78c95716e45a4ad924a44fe53ba168d345497cff
Luis Montes! <luis.montesgz@udlap.mx>
2019-11-20 14:35:32 -0600

    CREATE creo que Done

--- js/crudSP.js
+++ js/crudSP.js
@@ -4,30 +4,36 @@ $(function(){
         //var userid = _spPageContextInfo.userId;
         //var requestUri = _spPageContextInfo.webAbsoluteUrl + "/_api/web/getuserbyid(" + userid + ")";
         
-        var title = document.getElementById('Title').value;
+        var title = document.getElementById('title').value ;
         var nombre = document.getElementById('nombre').value;
         var date = document.getElementById('fechaNacimiento').value;
         var number = document.getElementById('telefono').value;
 
-        var requestUri = siteURl + "/_api/lists/getByTitle('" + listTitle + "')";
+        console.log(title);
+        console.log(nombre);
+        console.log(date);
+        console.log(number);
+
+        var requestUri = siteURl + "/_api/lists/getByTitle('TestOficial')";
         var requestHeaders = { 
             "accept": "application/json;odata=verbose",
             '__metadata': {
                 'type': 'SP.Data.listname.ListItem'
             },
-            'Title': + title,
-            'Nombre': + nombre,
-            'fechaNacimiento': + fechaNacimiento,
-            'Number': + number 
+            'Title': title ,
+            'Nombre':  nombre,
+            'fechaNacimiento': date,
+            'Number': number 
           };  
-    print(requestHeaders);
-    /*$.ajax({
+    console.log(requestHeaders);
+    $.ajax({
         url: requestUri,
+        method: "POST",
         contentType: "application/json;odata=verbose",
         headers: requestHeaders,
         success: onSuccess,        
         error: onError    
-    }); */
+    });
     function onSuccess(data, request) {        
         var userinfo = data.d;               
         alert ('Display Name: '+ userinfo.Title);  
